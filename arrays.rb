4d5dc12382b6f54f0a03975fee048de14c955cfb
Jessica <jessicaesimpson91@gmail.com>
2017-07-03 15:01:40 -0500

    first commit

---
+++ arrays.rb
@@ -0,0 +1,25 @@
+a = [1, 2, 5, 4, 3]
+
+#puts a.at(1)
+
+#puts a.fetch(-2)
+
+#a.delete(3)
+#puts a
+
+#puts a.reverse
+
+#puts a.length
+
+#puts a.sort 
+
+#a.slice!(0)
+#puts a
+
+#puts a.shuffle 
+
+#puts a.join(" * ")
+
+#puts a.insert 2, 12
+
+#puts a.values_at(0,2).join(' to ')
